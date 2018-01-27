ce4ccb8f278dedc8d5658bcfe9306de33383981f
FeliZorIs <32490220+FeliZorIs@users.noreply.github.com>
2018-01-27 00:04:37 -0500

    health test set up
    -Set two players on opposite sides
    -Set the images for health
    -Black hearts are stand ins for projectiles
    -They will drop and cause each player to lose health (that is next)

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player 2")
+        {
+            Debug.Log("Hit it. Fade it. Gone");
+            Destroy(this.gameObject);
+        }
+        Destroy(this.gameObject);
+    }
+
+}
